be3c160d1c07224cd2be02dd1639dfc780403035
nik1olay <85620548+nik1olay@users.noreply.github.com>
2021-09-23 13:35:33 +0300

    Initial Commit

---
+++ TableView+MVC+DZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView+MVC+DZ
+//
+//  Created by Cергей Иванович on 23.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
